234f49660d4a8810a484630a3c4eb9c6d9718cde
Benroberts37 <benrobsemail@gmail.com>
2022-04-15 11:17:49 -0400

    Thought I was close but still have 10 tests failing

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,34 @@
 // add middlewares here related to projects
+const Projects = require('./projects-model')
+
+function validateProjectId(req, res, next){
+    Projects.get(req.params.id)
+    .then(project => {
+        if (project) {
+        req.project = project
+        next()
+    } else {
+        res.status(404).json({message: "Project not found"})
+    }
+ })
+}
+
+function validateProjectInput(req, res, next){
+    const { name, description } = req.body
+    if (!name || !description) {
+        res.status(400).json({message: "Name and description field are both required, please include!"})
+    } else {
+        next()
+    }
+}
+
+function validateProjectUpdate(req, res, next){
+    const {name, description, completed} = req.body
+    if (!name || !description || !completed) {
+        res.status(400).json({message: "Name, description, and completed status are all required"})
+    } else {
+        next()
+    }
+}
+
+module.exports = { validateProjectId, validateProjectInput, validateProjectUpdate } 
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -4,6 +4,7 @@ const express = require("express");
 const router = express.Router()
 
 const Projects = require("./projects-model");
+const {validateProjectId, validateProjectInput, validateProjectUpdate } = require('./projects-middleware')
 
 
 router.get("/", (req, res) => {
@@ -15,20 +16,28 @@ router.get("/:id", (req, res) => {
     res.json(req.project)
 })
 
-router.post("/", (req, res) => {
-
+router.post("/", validateProjectInput, (req, res) => {
+    Projects.insert(req.body)
+    .then(project => res.status(201).json(project))
 })
 
-router.put("/", (req, res) => {
-
+router.put("/", validateProjectId, validateProjectUpdate, (req, res) => {
+    const id = req.params.id
+    const updates = req.body
+    Projects.update(id, updates)
+    .then (newProject => res.json(newProject))
 })
 
-router.delete("/", (req, res) => {
-
+router.delete("/", validateProjectId, (req, res) => {
+    const id = req.params.id
+    Projects.remove(id)
+    .then(() => res.end())
 })
 
 router.get("/", (req, res) => {
-
+    const projectId = req.params.id
+    Projects.getProjectActions(projectId)
+        .then(actions => res.json(actions))
 })
 
 

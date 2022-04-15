21f37063cebc36495e469e6822c76197b10cc09c
Benroberts37 <benrobsemail@gmail.com>
2022-04-15 06:34:32 -0400

    almost done with the actions.. stuck on post. then need to do projects

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,34 @@
 // add middlewares here related to actions
+const Actions = require('./actions-model')
+
+function validateActionId(req, res, next) {
+    Actions.get(req.params.id)
+        .then(action => {
+            if (action) {
+                req.action=action
+                next()
+            } else {
+                res.status(404).json({message: "Action not found" })
+            }
+        })
+}
+
+function validateActionInput(req, res, next) {
+    const {notes, description, project_id} = req.body
+    if (!notes || !description || !project_id) {
+        res.status(400).json({message: "Description, Notes, and Project_Id are all required for posts"})
+    } else {
+        next()
+    }
+}
+
+function validateActionUpdate(req, res, next) {
+    const {description, notes, project_id, completed} = req.body
+    if (!description || !notes || !project_id || completed === undefined) {
+        res.status(400).json({message: "Description, Notes, Project_Id, and Completed status are required for updates"})
+    } else {
+        next()
+    }
+}
+
+module.exports = { validateActionId, validateActionInput, validateActionUpdate} 
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,8 +1,9 @@
 // Write your "actions" router here!
 const express = require("express");
-const Actions = require("./actions-model");
-
 const router = express.Router();
+
+const Actions = require("./actions-model");
+const {validateActionId, validateActionInput, ValidateActionUpdate, validateActionUpdate} = require('./actions-middlware');
 
 
 router.get("/", (req, res) => {
@@ -10,21 +11,31 @@ Actions.get()
     .then(actions => res.json(actions))
 })
 
-router.get("/", (req, res) => {
-
+router.get("/:id", validateActionId, (req, res) => {
+    const { id } = req.params
+    Actions.get(id)
+        .then(action => 
+            res.json(action))
 })
 
 router.post("/", (req, res) => {
 
 })
 
-router.put("/", (req, res) => {
-
+router.put("/:id", validateActionInput, validateActionUpdate, (req, res) => {
+    const actionId = req.params.id
+    const {notes, description, project_id, completed} = req.body
+    const newActionReq = {notes, description, project_id, completed}
+    Actions.update(actionId, newActionReq)
+        .then(newActionReq => res.status(200).json(newActionReq))
 })
 
-router.delete("/", (req, res) => {
-
+router.delete("/", validateActionId, (req, res) => {
+    const id = req.params.id
+    Actions.remove(id)
 })
+
+module.exports = router
 
 
 
